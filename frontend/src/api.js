b0c2bb51aeef859e7b3634a754ea17fad2bb05cf
Gul2108 <kumargulshan.2108@gmail.com>
2025-12-09 11:35:14 +0530

    final deploy changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api/sales";
+const API_BASE_URL = "https://truestate-assignment-822j.onrender.com/api";
 
 export const fetchSales = async (params) => {
-  const response = await axios.get(API_BASE, { params });
+  const response = await axios.get(`${API_BASE_URL}/sales`, { params });
+  console.log("API Response:", response.data); // Debugging line
   return response.data;
 };
